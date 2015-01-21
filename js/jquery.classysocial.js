5cd35cf0a9f6e3eb6d84bae7eb96699f5f03e963
pupi1985 <pupi1985@bitbucket.org>
2015-01-21 00:20:47 -0300

    Removed support for HTML5 attributes in favor of JSON options. Added texts customization from options. Added ability to add new networks from the options. Removed the need to add the duplicated data-networks and each data-<network>-<handle> option. Main button image can be easily set from options when not fetched from a social network. Added ability to fetch the main button image from any network.

--- js/jquery.classysocial.js
+++ js/jquery.classysocial.js
@@ -3,156 +3,164 @@
  * www.class.pm
  *
  * Written by Marius Stanciu - Sergiu <marius@class.pm>
+ * Extended by Gabriel Zanetti - http://github.com/pupi1985
  * Licensed under the MIT license www.class.pm/LICENSE-MIT
- * Version 2.2.0
+ * Version pupi1985-2.3.0
  *
  */
 (function($) {
-    $.fn.ClassySocial = function() {
-        var ClassySocial = function(element) {
-            this.profiles = {
-                facebook: {
-                    url: 'http://www.facebook.com/{handle}',
-                    name: 'Facebook',
-                    tip: 'View my Facebook page'
+    $.fn.ClassySocial = function(userOptions) {
+        var ClassySocial = function(element, userOptions) {
+            var defaultOptions = {
+                profiles: {
+                    facebook: {
+                        url: 'http://www.facebook.com/{handle}',
+                        tip: 'View my Facebook page',
+                        imageUrl: 'https://graph.facebook.com/{handle}/picture?type=large'
+                    },
+                    google: {
+                        url: 'https://plus.google.com/{handle}',
+                        tip: 'View my Google Plus profile'
+                    },
+                    twitter: {
+                        url: 'https://twitter.com/{handle}',
+                        tip: 'Check out my Twitter profile'
+                    },
+                    pinterest: {
+                        url: 'http://pinterest.com/{handle}',
+                        tip: 'View my Pinterest profile'
+                    },
+                    linkedin: {
+                        url: 'http://www.linkedin.com/profile/view?id={handle}',
+                        tip: 'View my LinkedIn profile'
+                    },
+                    dribbble: {
+                        url: 'http://dribbble.com/{handle}',
+                        tip: 'View my Dribble page'
+                    },
+                    email: {
+                        url: 'mailto:{handle}',
+                        tip: 'Send me an e-mail'
+                    },
+                    socl: {
+                        url: 'http://www.so.cl/#/profile/{handle}',
+                        tip: 'View my Socl profile'
+                    },
+                    instagram: {
+                        url: 'http://instagram.com/{handle}',
+                        tip: 'View my Instagram profile'
+                    },
+                    vimeo: {
+                        url: 'http://vimeo.com/{handle}',
+                        tip: 'View my Vimeo profile'
+                    },
+                    youtube: {
+                        url: 'http://www.youtube.com/user/{handle}',
+                        tip: 'Check out my Youtube profile'
+                    },
+                    github: {
+                        url: "https://www.github.com/{handle}",
+                        tip: 'View my GitHub developer profile'
+                    },
+                    blogger: {
+                        url: "http://www.blogger.com/profile/{handle}",
+                        tip: 'View my Blogger profile'
+                    },
+                    deviantart: {
+                        url: 'https://{handle}.deviantart.com',
+                        tip: 'Check out my DeviantArt page'
+                    },
+                    flickr: {
+                        url: 'http://www.flickr.com/photos/{handle}',
+                        tip: 'View my Flickr images'
+                    },
+                    skype: {
+                        url: 'skype:{handle}?userinfo',
+                        tip: 'Call me on Skype'
+                    },
+                    steam: {
+                        url: 'http://steamcommunity.com/profiles/{handle}',
+                        tip: 'View my Steam profile'
+                    },
+                    wordpress: {
+                        url: 'http://{handle}.wordpress.com',
+                        tip: 'View my wordpress.com profile'
+                    },
+                    yahoo: {
+                        url: 'http://profile.yahoo.com/y/pulse/{handle}',
+                        tip: 'Check out my Yahoo profile'
+                    }
                 },
-                google: {
-                    url: 'https://plus.google.com/{handle}',
-                    name: 'Google Plus',
-                    tip: 'View my Google Plus profile'
+                mainButton: {
+                    tip: 'Find me here!',
+                    image: {
+                        type: 'default',  // Can be 'profile or 'default'
+                        name: 'facebook'  // Only needed when type is 'profile'. Requires 'imageUrl' set in the given profile
+                    },
                 },
-                twitter: {
-                    url: 'https://twitter.com/{handle}',
-                    name: 'Twitter',
-                    tip: 'Check out my Twitter profile'
-                },
-                pinterest: {
-                    url: 'http://pinterest.com/{handle}',
-                    name: 'Pinterest',
-                    tip: 'View my Pinterest profile'
-                },
-                linkedin: {
-                    url: 'http://www.linkedin.com/profile/view?id={handle}',
-                    name: 'LinkedIn',
-                    tip: 'View my LinkedIn profile'
-                },
-                dribbble: {
-                    url: 'http://dribbble.com/{handle}',
-                    name: 'Dribbble',
-                    tip: 'View my Dribble page'
-                },
-                email: {
-                    url: 'mailto:{handle}',
-                    name: 'Email',
-                    tip: 'Send me an e-mail'
-                },
-                socl: {
-                    url: 'http://www.so.cl/#/profile/{handle}',
-                    name: 'Socl',
-                    tip: 'View my Socl profile'
-                },
-                instagram: {
-                    url: 'http://instagram.com/{handle}',
-                    name: 'Instagram',
-                    tip: 'View my Instagram profile'
-                },
-                vimeo: {
-                    url: 'http://vimeo.com/{handle}',
-                    name: 'Vimeo',
-                    tip: 'View my Vimeo profile'
-                },
-                youtube: {
-                    url: 'http://www.youtube.com/user/{handle}',
-                    name: 'YouTube',
-                    tip: 'Check out my Youtube profile'
-                },
-                github: {
-                    url: "https://www.github.com/{handle}",
-                    name: 'GitHub',
-                    tip: 'View my GitHub developer profile'
-                },
-                blogger: {
-                    profile_url: "http://www.blogger.com/profile/{handle}",
-                    name: 'Blogger',
-                    tip: 'View my Blogger profile'
-                },
-                deviantart: {
-                    url: 'https://{handle}.deviantart.com',
-                    name: 'DeviantArt',
-                    tip: 'Check out my DeviantArt page'
-                },
-                flickr: {
-                    url: 'http://www.flickr.com/photos/{handle}',
-                    name: 'Flickr',
-                    tip: 'View my Flickr images'
-                },
-                skype: {
-                    url: 'skype:{handle}?userinfo',
-                    name: 'Skype',
-                    tip: 'Call me on Skype'
-                },
-                steam: {
-                    url: 'http://steamcommunity.com/profiles/{handle}',
-                    name: 'Steam',
-                    tip: 'View my Steam profile'
-                },
-                wordpress: {
-                    url: 'http://{handle}.wordpress.com',
-                    name: 'Wordpress',
-                    tip: 'View my wordpress.com profile'
-                },
-                yahoo: {
-                    url: 'http://profile.yahoo.com/y/pulse/{handle}',
-                    name: 'Yahoo',
-                    tip: 'Check out my Yahoo profile'
-                }
+                theme: 'default',
+                orientation: 'arc',
+                arcStart: 0,
+                arcLength: 360,
+                arcRadius: 80,
+                gap: 80
             };
 
+            var options = {};
+            $.extend(true, options, defaultOptions, userOptions);
+
             var el = $(element);
-            var networks = el.attr('data-networks') ? el.attr('data-networks').toLowerCase().split(",") : undefined;
-            var orientation = el.attr('data-orientation') ? el.attr('data-orientation').toLowerCase() : 'arc';
-            var arcStart = el.attr('data-arc-start') ? parseInt(el.attr('data-arc-start')) : 0;
-            var arcLength = el.attr('data-arc-length') ? parseInt(el.attr('data-arc-length')) : 180;
-            var radius = el.attr('data-radius') ? parseInt(el.attr('data-radius')) : 80;
-            var gap = el.attr('data-gap') ? parseInt(el.attr('data-gap')) : 50;
-            var picture = el.attr('data-picture') ? el.attr('data-picture') : 'none';
-            var imageType = el.attr('data-image-type') ? el.attr('data-image-type').toLowerCase() : 'picture';
-            var theme = el.attr('data-theme') ? el.attr('data-theme').toLowerCase() : 'default';
+            var imageUrl = null;
 
             this.__constructor = function() {
                 el.addClass('bubble');
                 var template = this.template;
-                if (imageType === 'facebook') {
-                    picture = 'https://graph.facebook.com/' + picture + '/picture?type=large';
+                var containerTemplate = template.containerWithoutProfileImage;
+                if (options.mainButton.image.type === 'profile') {
+                    var profileId = options.mainButton.image.name;
+                    var profileImage = options.profiles[profileId].imageUrl;
+                    if (profileImage) {
+                        imageUrl = profileImage.replace(/\{handle\}/gi, options.profiles[profileId].handle);
+                        containerTemplate = template.containerWithProfileImage;
+                    }
                 }
-                el.html(template["container"].replace(/\{imageURL\}/gi, picture));
+                el.html(containerTemplate
+                    .replace(/\{mainButtonImageUrl\}/gi, imageUrl)
+                    .replace(/\{theme\}/gi, options.theme)
+                    .replace(/\{mainButtonTip\}/gi, options.mainButton.tip)
+                );
                 var container = el.find(".scontainer");
-                for (var t = 0; t < networks.length; t++) {
-                    var i = networks[t], o = this.profiles[i], u;
-                    if (o['url'] && el.attr('data-' + i + '-handle')) {
-                        u = o['url'].replace(/\{handle\}/gi, el.attr('data-' + i + '-handle'));
+                $.each(options.profiles, function(profileKey, profile){
+                    if (profile.url && profile.handle) {
+                        var u = profile.url.replace(/\{handle\}/gi, profile.handle);
+                        var a = template.button
+                            .replace(/\{url\}/gi, u)
+                            .replace(/\{tip\}/gi, profile.tip)
+                            .replace(/\{network\}/gi, profileKey)
+                        ;
+                        container.append($(a));
                     }
-                    else {
-                        continue;
-                    }
-                    var a = template['button'].replace(/\{URL\}/gi, u).replace(/\{tip\}/gi, o['tip']).replace(/\{name\}/gi, o['name']).replace(/\{network\}/gi, i), f = $(a);
-                    container.append(f);
-                }
+
+                });
                 return this._build(template);
             };
 
             this._build = function(template) {
-                var tpl = template['orientations'][orientation] ? template['orientations'][orientation] : template['orientations']['arc'];
-                el.find('.smain').click(tpl);
+                var orientationFunction = template.orientations[options.orientation];
+                el.find('.smain').click(orientationFunction);
                 return this;
             };
 
             this.template = {
-                container: '<a class="smain ' + theme + '" title="Find Me Here" alt="Find Me Here">' +
-                        '<img src="{imageURL}" title="Find Me Here" alt="Find Me Here"/>' +
-                        '</a>' +
-                        '<div class="scontainer ' + theme + '"></div>',
-                button: '<a class="sbutton {network}" title="{tip}" target="_blank" href="{URL}" data-network="{network}">{name}</a>',
+                containerWithProfileImage:
+                    '<a class="smain {theme}" title="{mainButtonTip}" alt="{mainButtonTip}">' +
+                        '<img src="{mainButtonImageUrl}" title="{mainButtonTip}" alt="{mainButtonTip}"/>' +
+                    '</a>' +
+                    '<div class="scontainer {theme}"></div>',
+                containerWithoutProfileImage:
+                    '<span class="smain {theme} default-image" title="{mainButtonTip}" alt="{mainButtonTip}"></span>' +
+                    '<div class="scontainer {theme}"></div>',
+                button: '<a class="sbutton {network}" title="{tip}" target="_blank" href="{url}"></a>',
                 orientations: {
                     arc: function() {
                         if ($(this).hasClass('disabled')) {
@@ -167,9 +175,9 @@
                                 $(this).removeClass('disabled').addClass('active');
                                 e();
                             });
-                            var v = arcLength / r, m = arcStart + v / 2;
+                            var v = options.arcLength / r, m = options.arcStart + v / 2;
                             el.find('.sbutton').each(function() {
-                                var n = m / 180 * Math.PI, r = p + radius * Math.cos(n), i = d + radius * Math.sin(n);
+                                var n = m / 180 * Math.PI, r = p + options.arcRadius * Math.cos(n), i = d + options.arcRadius * Math.sin(n);
                                 $(this).css({
                                     display: 'block',
                                     left: p + 'px',
@@ -181,8 +189,7 @@
                                 m += v;
                                 s++;
                             });
-                        }
-                        else {
+                        } else {
                             s = r - 1;
                             $(this).addClass('disabled').delay(i).queue(function(e) {
                                 $(this).removeClass('disabled').removeClass('active');
@@ -201,10 +208,10 @@
                         if ($(this).hasClass('disabled')) {
                             return;
                         }
-                        var e = 500, t = 250, r = el.find('.sbutton').length, i = gap, s = e + (r - 1) * t, o = 1;
+                        var e = 500, t = 250, r = el.find('.sbutton').length, i = options.gap, s = e + (r - 1) * t, o = 1;
                         var a = $(this).outerWidth(), f = $(this).outerHeight();
                         var c = el.find('.sbutton:eq(0)').outerWidth(), h = el.find('.sbutton:eq(0)').outerHeight();
-                        var p = (a - c) / 2, d = (f - h) / 2, v = arcStart / 180 * Math.PI;
+                        var p = (a - c) / 2, d = (f - h) / 2, v = options.arcStart / 180 * Math.PI;
                         if (!$(this).hasClass('active')) {
                             $(this).addClass('disabled').delay(s).queue(function(e) {
                                 $(this).removeClass('disabled').addClass('active');
@@ -222,8 +229,7 @@
                                 }, e);
                                 o++;
                             });
-                        }
-                        else {
+                        } else {
                             o = r;
                             $(this).addClass('disabled').delay(s).queue(function(e) {
                                 $(this).removeClass('disabled').removeClass('active');
@@ -242,10 +248,10 @@
                         if ($(this).hasClass('disabled')) {
                             return;
                         }
-                        var e = 500, t = 250, r = el.find('.sbutton').length, i = gap, s = e + (r - 1) * t, o = 1;
+                        var e = 500, t = 250, r = el.find('.sbutton').length, i = options.gap, s = e + (r - 1) * t, o = 1;
                         var a = $(this).outerWidth(), f = $(this).outerHeight();
                         var c = el.find('.sbutton:eq(0)').outerWidth(), h = el.find('.sbutton:eq(0)').outerHeight();
-                        var p = (a - c) / 2, d = (f - h) / 2, v = arcStart / 180 * Math.PI;
+                        var p = (a - c) / 2, d = (f - h) / 2, v = options.arcStart / 180 * Math.PI;
                         if (!$(this).hasClass('active')) {
                             $(this).addClass('disabled').delay(s).queue(function(e) {
                                 $(this).removeClass('disabled').addClass('active');
@@ -263,8 +269,7 @@
                                 }, e);
                                 o++;
                             });
-                        }
-                        else {
+                        } else {
                             o = r;
                             $(this).addClass('disabled').delay(s).queue(function(e) {
                                 $(this).removeClass('disabled').removeClass('active');
@@ -284,7 +289,7 @@
             return this.__constructor();
         };
         return this.each(function() {
-            return new ClassySocial(this);
+            return new ClassySocial(this, userOptions);
         });
     };
 })(jQuery);
